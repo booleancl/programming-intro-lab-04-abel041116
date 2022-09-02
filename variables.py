ba0172c207ef21e3496a5373bc71c626f10b3830
Abel <abel.valverde041116@gmail.com>
2022-09-02 11:08:21 -0400

    string python

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Una variable es un nombre que apunta a un valor
+# los valores son de cierto tipo
+
+message = "Esto es un mensaje"
+
+age = 31
+
+# los
+dolar_to_peso = 890.2
+
+print(message)
+print(age)
+print(dolar_to_peso)
